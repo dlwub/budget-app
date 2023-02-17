bd188daf8ecb4a361d7032ef9d177f807ceead83
dlwub <dlwubi@gmail.com>
2023-02-17 18:59:40 +0300

    Generate categories scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_132818) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_154610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "entities", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount"
-    t.string "category"
-    t.bigint "user_id"
+    t.string "icon"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_entities_on_user_id"
-  end
-
-  create_table "entity_groups", force: :cascade do |t|
-    t.bigint "entity_id"
-    t.bigint "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["entity_id"], name: "index_entity_groups_on_entity_id"
-    t.index ["group_id"], name: "index_entity_groups_on_group_id"
-  end
-
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.text "icon"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,9 +31,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_132818) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
 end
