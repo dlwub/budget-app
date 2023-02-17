2c9f8902476df712b4b58051d3546eade350f519
dlwub <dlwubi@gmail.com>
2023-02-17 19:16:12 +0300

    Generate transaction and category_transaction models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_154610) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_161340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_154610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_transactions", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_transactions_on_category_id"
+    t.index ["transaction_id"], name: "index_category_transactions_on_transaction_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.string "category"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_154610) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "category_transactions", "categories"
+  add_foreign_key "category_transactions", "transactions"
+  add_foreign_key "transactions", "users"
 end
---
+++ spec/views/transactions/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "transactions/show", type: :view do
+  before(:each) do
+    assign(:transaction, Transaction.create!(
+      name: "Name",
+      amount: "9.99",
+      category: "Category",
+      user: nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/9.99/)
+    expect(rendered).to match(/Category/)
+    expect(rendered).to match(//)
+  end
+end
