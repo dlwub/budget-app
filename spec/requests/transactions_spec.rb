006d1db931e836f835c292215480a31a95172b82
dlwub <dlwubi@gmail.com>
2023-02-17 20:14:45 +0300

    Fix linter errors

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -76,7 +76,6 @@ RSpec.describe '/transactions', type: :request do
         end.to change(Transaction, :count).by(0)
       end
 
-
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post transactions_url, params: { transaction: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
