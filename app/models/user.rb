30215b4dfbd3ec69174e05ffa0a33a6633ddc73c
dlwub <dlwubi@gmail.com>
2023-02-18 08:05:41 +0300

    Regenerate schema file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories
+  has_many :transactions
+  validates :full_name, presence: true
+  validates :email, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
--- db/migrate/20230217205547_create_category_transactions.rb
+++ db/migrate/20230217205547_create_category_transactions.rb
@@ -1,8 +1,8 @@
 class CreateCategoryTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :category_transactions do |t|
-      t.references :category, null: false, foreign_key: true
-      t.references :transaction, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+      t.belongs_to :transaction, null: false, foreign_key: true
 
       t.timestamps
     end
